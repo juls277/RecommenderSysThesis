95e20b45a6293197514f88c72a67a33878f63979
juls27 <89221003@student.upr.si>
2025-11-04 11:57:47 +0100

    additional event listener to see session stored data

--- main/public/app.js
+++ main/public/app.js
@@ -1,11 +1,18 @@
 const startTime = Date.now();
-    //fecth movies 
+
+//additional listener here so session data is always visible during development
+window.addEventListener('load', () => {
+  const session = JSON.parse(localStorage.getItem('sessionHistory'));
+  console.log('Current session on load:', session);
+});
+
+//fecth movies 
     fetch('/movies')
   .then(res => res.json())
   .then(movies => {
     const grid = document.getElementById('movie-list');
-  
-    //i added this slicing here cos my brother constanly freezes if it loads all data set with layouts and stuff
+    
+    //i added this slicing here cos my browser constanly freezes if it loads all data set with layouts and stuff
     movies.slice(0,100).forEach(movie => {
       const card = document.createElement('div');
       card.className = 'card';
@@ -24,6 +31,7 @@ const startTime = Date.now();
       card.appendChild(title);
       grid.appendChild(card);
 
+     
       // create and save session
       card.addEventListener('click', () => {
         const secondsSpent = Math.floor((Date.now() - startTime) / 1000);
@@ -36,13 +44,26 @@ const startTime = Date.now();
 
         session.visitedItems.push(movie.id);
         session.clicks.push(`item_${movie.id}`);
-        session.timeSpent[`item_${movie.id}`] = secondsSpent;
+        //localStorage.setItem('sessionHistory', JSON.stringify(session));
 
         localStorage.setItem('sessionHistory', JSON.stringify(session));
         console.log('Saved session:', session);
 
+        // store timestamp for tracking time on details page
+        localStorage.setItem('currentMovie', JSON.stringify({
+          id: movie.id,
+          openTime: Date.now()
+        }));
+        
+
+        window.location.href = `/movie.html?id=${movie.id}`;
+
+        
+
+
         
       });
+      
     });
   })
   .catch(error => {
